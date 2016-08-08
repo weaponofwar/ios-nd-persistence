15b6ad271dcc2aff97392b87792786abd73711b6
James Taylor <jamestaylortx@gmail.com>
2016-08-08 14:49:02 -0700

    Add project CoolNoteJames

---
+++ CoolNotesJames/CoolNotesJames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoolNotesJames
+//
+//  Created by James Taylor on 8/8/16.
+//  Copyright © 2016 James Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
